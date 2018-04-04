623f674fb12e25bc8ca3552f03eb02d060ac1eac
Aaron Kling <webgeek1234@gmail.com>
2018-04-03 22:02:47 -0500

    Fix android jni compile on clang

--- libretro/jni/Android.mk
+++ libretro/jni/Android.mk
@@ -29,7 +29,7 @@ LOCAL_MODULE    := libretro
 LOCAL_SRC_FILES = $(SOURCES_CXX)
 LOCAL_CXXFLAGS += -DINLINE=inline -DLSB_FIRST -D__LIBRETRO__ -DFRONTEND_SUPPORTS_RGB565 -I$(CORE_DIR)/ameteor/include
 LOCAL_C_INCLUDES += external/stlport/stlport bionic $(CORE_DIR)/ameteor/include
-LOCAL_LD_LIBS += -latomic
+LOCAL_LDLIBS += -latomic
 
 # https://code.google.com/p/android-ndk-profiler/
 # Example: ndk-build NDK_MODULE_PATH=.../android-ndk-profiler-prebuilt-3.3 APP_ABI=armeabi-v7a PROFILE=1
